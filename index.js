b6894a28613da5aeee957dc765be5b6646a5d6da
merlin <Merlin.Chacko@hrblock.com>
2026-07-29 15:33:29 +0530

    commit

--- index.js
+++ index.js
@@ -3,7 +3,7 @@ const express = require("express");
 const app = express();
 
 app.get("/", (req, res) => {
-  res.send("Hello CI/CD Application is Running!");
+  res.send("Hello CI/CD Application is Running successfully!");
 });
 
 module.exports = app;
